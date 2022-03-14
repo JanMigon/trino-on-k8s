97fda5a410e4fffa5aa6b04f47e9c9cf828cdd65
Jan Migon <janmigon@Jans-MBP.localdomain>
2022-03-14 10:01:10 +0100

    Add bitnami postgres chart and minio

---
+++ bitnami-postgresql/files/docker-entrypoint-initdb.d/dashboard-data-dump.sql
@@ -0,0 +1,71 @@
+INSERT INTO common_function_sql (id, name) VALUES
+(DEFAULT, 'common_function1'),
+(DEFAULT,'common_function2');
+
+INSERT INTO data_source_sql (id, name) VALUES
+(DEFAULT, 'data_source1'),
+(DEFAULT,'data_source2');
+
+INSERT INTO department_sql (id, name) VALUES
+(DEFAULT, 'department1'),
+(DEFAULT,'department2');
+
+INSERT INTO frontend_technology_sql (id, name) VALUES
+(DEFAULT, 'frontend_technology1'),
+(DEFAULT,'frontend_technology2');
+
+INSERT INTO hierarchy_sql (id, name) VALUES
+(DEFAULT, 'hierarchy1'),
+(DEFAULT,'hierarchy2');
+
+INSERT INTO integrated_portal_sql (id, name) VALUES
+(DEFAULT, 'integrated_portal1'),
+(DEFAULT,'integrated_portal2');
+
+INSERT INTO kpi_name_sql (id, name) VALUES
+(DEFAULT, 'kpi_name1'),
+(DEFAULT,'kpi_name2');
+
+INSERT INTO product_phase_sql (id, name) VALUES
+(DEFAULT, 'product_phase1'),
+(DEFAULT,'product_phase2');
+
+INSERT INTO query_sql (id, name) VALUES
+(DEFAULT, 'query1'),
+(DEFAULT,'query2');
+
+INSERT INTO reporting_cause_sql (id, name) VALUES
+(DEFAULT, 'reporting_cause1'),
+(DEFAULT,'reporting_cause2');
+
+INSERT INTO ressort_sql (id, name) VALUES
+(DEFAULT, 'ressort1'),
+(DEFAULT,'ressort2');
+
+INSERT INTO specific_function_sql (id, name) VALUES
+(DEFAULT, 'specific_function1'),
+(DEFAULT,'specific_function2');
+
+INSERT INTO status_sql (id, name) VALUES
+(DEFAULT, 'status1'),
+(DEFAULT,'status2');
+
+INSERT INTO design_guide_sql (id, name) VALUES
+(DEFAULT, 'design_guide1'),
+(DEFAULT,'design_guide2');
+
+INSERT INTO agile_releasetrain_sql (id, name) VALUES
+(DEFAULT, 'agile_release_train1'),
+(DEFAULT,'agile_release_train2');
+
+INSERT INTO subsystem_sql (id, name) VALUES
+(DEFAULT, 'subsystem1'),
+(DEFAULT,'subsystem2');
+
+INSERT INTO connection_type_sql (id, name) VALUES
+(DEFAULT, 'connectiontype1'),
+(DEFAULT,'connectiontype2');
+
+INSERT INTO data_warehouse_sql (id, name) VALUES
+(DEFAULT, 'datawarehouse1'),
+(DEFAULT,'datawarehouse2');
